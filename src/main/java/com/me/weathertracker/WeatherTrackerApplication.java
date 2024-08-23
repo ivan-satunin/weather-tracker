1d8d2e1cc5302c3c58ddb49662235d20862b58ff
Ivan Satunin <satuninivan@gmail.com>
2024-08-23 20:20:29 +0300

    Initial commit

---
+++ src/main/java/com/me/weathertracker/WeatherTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.me.weathertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/weathertracker/WeatherTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.weathertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
